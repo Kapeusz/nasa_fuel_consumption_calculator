4f3b67e615052aeb17a7ad41b932c5ec9117aac5
MKacper <kacpermuryn@outlook.com>
2022-04-28 15:32:57 +0200

    Project initialization

---
+++ lib/nasa_fuel_consumption_calculator.ex
@@ -0,0 +1,18 @@
+defmodule NasaFuelConsumptionCalculator do
+  @moduledoc """
+  Documentation for `NasaFuelConsumptionCalculator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NasaFuelConsumptionCalculator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nasa_fuel_consumption_calculator_test.exs
@@ -0,0 +1,8 @@
+defmodule NasaFuelConsumptionCalculatorTest do
+  use ExUnit.Case
+  doctest NasaFuelConsumptionCalculator
+
+  test "greets the world" do
+    assert NasaFuelConsumptionCalculator.hello() == :world
+  end
+end
